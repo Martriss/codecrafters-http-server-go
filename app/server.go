f522eeb0e7814bf9493f790140a482730cdc590d
Martin Risser <mrisser@deezer.com>
2024-06-06 16:53:11 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,28 +2,42 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port 4221: ", err)
 		os.Exit(1)
 	}
+	defer l.Close()
+	fmt.Println("Listening...")
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		buf := make([]byte, 512)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		req := string(buf)
+
+		target := strings.Split(req, " ")[1]
+		res := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		if target == "/" {
+			res = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		}
+		conn.Write(res)
+		conn.Close()
 	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
